7fc6e382954b056cdf223fb13c7fe50a19bb8738
Ivan Kasatenko <sky.31338@gmail.com>
2012-06-04 20:19:53 +0400

    Created the Rails 2.3.5 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RntdDatabase::Application.config.secret_token = 'a3f2ffd9eba5079f565a34b2cbd6b7cc763b320188b07baab182106950885528620b192ba188385a7c4f10fc3b382e862019f18f116e58d26467e107c65ca7f3'
